d9cd481fc8ca6895d1f835fe96647749c7134ab0
shiv <defi.shiv@gmail.com>
2024-09-29 16:18:24 +0530

    login/signup responsive

--- frontend/src/Signup.tsx
+++ frontend/src/Signup.tsx
@@ -11,50 +11,53 @@ import { useNavigate } from "react-router-dom";
 function Signup() {
     const navigate = useNavigate();
     return (
-        <div className="flex w-[100vw] h-[100vh] ">
-            <div className="h-full w-1/4 bg-slate-950 flex flex-col items-center py-28 px-10">
+        <div className="flex flex-col-reverse overflow-y-auto w-[100vw] h-[100vh] xl:flex-row bg-slate-950">
+            <div className="w-full py-10 bg-slate-950 hidden xl:flex xl:flex-col xl:items-center xl:py-28 xl:px-10 xl:h-full xl:w-1/4">
                 <div className="w-full mb-20 flex justify-center">
                     <img
-                        className="h-10 w-auto"
+                        className="h-7 xl:h-10 w-auto"
                         src="/vaultx.png"
                         alt="vaultx"
                         onClick={() => navigate("/")}
                     />
                 </div>
-                <div className=" font-bold w-full text-left mb-1 ">FAQs</div>
-                <Accordion type="single" collapsible className="w-full">
-                    <AccordionItem value="item-1">
-                        <AccordionTrigger>
-                            What cryptocurrencies does this platform support ?
-                        </AccordionTrigger>
-                        <AccordionContent>
-                            We support Bitcoin, Ethereum & Solana.
-                        </AccordionContent>
-                    </AccordionItem>
-                    <AccordionItem value="item-2">
-                        <AccordionTrigger>
-                            Can I deposit my crypto here ?
-                        </AccordionTrigger>
-                        <AccordionContent>
-                            Yes, as long as it is either Bitcoin or Ethereum or
-                            Solana, you can safely deposit and transact using
-                            your own crypto.
-                        </AccordionContent>
-                    </AccordionItem>
-                    <AccordionItem value="item-3">
-                        <AccordionTrigger>
-                            Is this platform secure ?
-                        </AccordionTrigger>
-                        <AccordionContent>
-                            Yes, we are following all the security measures to
-                            ensure the safety of your assets both during a
-                            transaction and when it sits idle in your wallet.
-                        </AccordionContent>
-                    </AccordionItem>
-                </Accordion>
+                <div>
+                    <div className=" font-bold w-full text-left mb-1">FAQs</div>
+                    <Accordion type="single" collapsible className="w-full">
+                        <AccordionItem value="item-1">
+                            <AccordionTrigger>
+                                What cryptocurrencies do we support ?
+                            </AccordionTrigger>
+                            <AccordionContent>
+                                We support Bitcoin, Ethereum & Solana.
+                            </AccordionContent>
+                        </AccordionItem>
+                        <AccordionItem value="item-2">
+                            <AccordionTrigger>
+                                Can I deposit my crypto here ?
+                            </AccordionTrigger>
+                            <AccordionContent>
+                                Yes, as long as it is either Bitcoin or Ethereum
+                                or Solana, you can safely deposit and transact
+                                using your own crypto.
+                            </AccordionContent>
+                        </AccordionItem>
+                        <AccordionItem value="item-3">
+                            <AccordionTrigger>
+                                Is this platform secure ?
+                            </AccordionTrigger>
+                            <AccordionContent>
+                                Yes, we are following all the security measures
+                                to ensure the safety of your assets both during
+                                a transaction and when it sits idle in your
+                                wallet.
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+                </div>
             </div>
-            <div className="h-full w-3/4 bg-slate-900 flex items-center justify-center">
-                <div className="w-[420px] border-2 border-slate-800 bg-slate-950 rounded-lg px-12 py-10">
+            <div className="w-full my-auto xl:h-full xl:w-3/4 bg-slate-900 flex items-center justify-center">
+                <div className="w-[420px] xl:border-2 xl:border-slate-800 bg-slate-950 rounded-lg px-12 py-10">
                     <div className="text-xl font-bold text-slate-200 w-full text-center mb-8">
                         Welcome aboard !
                     </div>
@@ -93,7 +96,11 @@ function Signup() {
                     </div>
                     <div className="w-full mt-10 flex flex-col">
                         <Button className="w-full py-6 mb-2">SIGN IN</Button>
-                        <Button className="w-full py-6" variant={"secondary"}>
+                        <Button
+                            className="w-full py-6"
+                            variant={"secondary"}
+                            onClick={() => navigate("/")}
+                        >
                             CANCEL
                         </Button>
                         <div className="w-full text-center text-slate-300 mt-4 text-sm">
